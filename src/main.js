dfc612bce5a5f01bedd5c28c4946d8bb9046c213
kunming100 <kim_tankunming@outlook.com>
2019-05-27 17:39:02 +0800

    第三次提交

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import 'lib-flexible'   //rem布局
+import fastclick from 'fastclick'
+import VueScroller from 'vue-scroller'
 
+Vue.use(VueScroller)
+fastclick.attach(document.body) //解决移动端点击300ms延时
 Vue.config.productionTip = false
-
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })

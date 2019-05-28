34e979a26f24965e2321fe7c3cd9fb52f10f5052
kunming100 <kim_tankunming@outlook.com>
2019-05-28 13:57:02 +0800

    引入sass

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      transitionName: 'slide-left'
+    }
+  },
+  // 监听路由的路径，可以通过不同的路径去选择不同的切换效果
+  watch: {
+    $route (to, from) {
+      if (to.meta.index > from.meta.index) {  //外 => 内
+        this.transitionName = 'slide-left'
+      } else if (to.meta.index < from.meta.index) {   //内 => 外
+        this.transitionName = 'slide-right'
+      } else {   //同级 无过渡效果
+        this.transitionName = ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './common/style/reset.scss';
+@import './common/style/mixins.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+// import 的方式实现懒加载
+// function loadView (view) {
+//   return () => import(`@/components/${view}.vue`)
+// }
+
+// require 的方式实现懒加载
+const helloWorld = r => require.ensure([], () => r(require('@/components/HelloWorld')), 'helloWorld')
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'helloWorld'
+    // },
+    // {
+    //   path: '/helloWorld',
+    //   name: 'helloWorld',
+    //   component: loadView('HelloWorld')
+    // },
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+    },
+    {
+      path: '/helloWorld',
+      meta: {
+        index: 1
+      },
+      component: helloWorld
     }
   ]
 })

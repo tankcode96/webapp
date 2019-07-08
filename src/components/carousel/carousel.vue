2bc82c15682e9fcf2f438c0a4dc8c4a489ec51ec
kunming100 <kim_tankunming@outlook.com>
2019-07-09 01:14:07 +0800

    修改轮播组件

--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="carousel" :style="{width: basis.width, height: basis.height}">
-    <div class="carousel-box" :style="{left: `${- (parseInt(basis.width) * currIndex)}rem`}" @touchStart="watchTouchStart" @touchEnd="watchTouchEnd">
+    <div
+      class="carousel-box"
+      :style="carouselStyle"
+      @touchstart.stop.prevent="watchTouchStart($event)"
+      @touchmove.stop.prevent="watchTouchMove($event)"
+      @touchend.stop.prevent="watchTouchEnd($event)">
       <template  v-for="(item, i) in carouselList">
         <router-link class="carousel-item" tag="div" :to="`/goods/${item.id}`" v-bind:key="i">
-          <img class="carousel-img" :src="require(`../../assets/images/${item.url}`)" :style="{width: basis.width, height: basis.height}" />
+          <img class="carousel-img" :src="require(`../../assets/images/${item.url}`)" :style="{width: basis.width, height: basis.height}" v-bind:key="i" />
         </router-link>
       </template>
     </div>
@@ -13,6 +18,10 @@
 <script>
   export default {
     name: 'Carousel',
+
+    time: {
+
+    },
 
     props: {
       list: {
@@ -28,30 +37,43 @@
     data () {
       return {
         currIndex: 1, // 当前展示的图片index
-        startX: 0,
-        endX: 0,
+        carouselStyle: 0,  // 轮播图box的style
+        startX: 0,  // 起始位置
+        currentX: 0,  // 当前位置
+        endX: 0,  // 离开位置
+        left: 0
       }
     },
 
     created () {
-
+      this.carouselStyle = {left: `${- (parseInt(this.basis.width) * this.currIndex)}rem`}
     },
 
     mounted () {
-      
     },
 
     computed: {
+
       /**
        * 构建轮播数组
        */
-     carouselList : function () {
+      carouselList () {
         const carouselList = this.list
         if (this.list && this.list.length > 0) {
           const len = this.list.length
           carouselList.unshift(this.list[len - 1])
         }
         return carouselList
+      }
+    },
+
+    watch: {
+      currentX () {
+        const html = document.getElementsByTagName('html')[0]
+        const distance = this.currentX - this.startX
+        this.carouselStyle = {
+          left: `${distance / parseInt(html.style.fontSize) - (parseInt(this.basis.width) * this.currIndex)}rem`
+        }
       }
     },
 
@@ -64,9 +86,18 @@
        * 记录触摸开始位置
        */
       watchTouchStart (e) {
-        e.preventDefault()
         if (e.touches.length === 1) {
           this.startX = e.touches[0].clientX
+          this.startTime = e.timeStamp
+        }
+      },
+
+      /**
+       * 记录触摸移动位置
+       */
+      watchTouchMove (e) {
+        if (e.touches.length === 1) {
+          this.currentX = e.touches[0].clientX
         }
       },
 
@@ -74,9 +105,11 @@
        * 记录触摸离开位置
        */
       watchTouchEnd (e) {
-        e.preventDefault()
         if (e.touches.length === 1) {
           this.endX = e.touches[0].clientX
+          this.endTime = e.timeStamp
+          const speed = Math.abs((this.endX - this.startX) / (this.endTime - this.startTime))
+          // if () {}
         }
       },
 
